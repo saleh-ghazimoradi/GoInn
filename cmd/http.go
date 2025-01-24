ba3676e121bb0abf80bde76112f6619ea054b843
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-24 18:55:24 +0330

    add cobra cli to the project

---
+++ cmd/http.go
@@ -0,0 +1,23 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// httpCmd represents the http command
+var httpCmd = &cobra.Command{
+	Use:   "http",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("http called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(httpCmd)
+}
---
+++ cmd/root.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "GoInn",
+	Short: "GoInn, a Hotel Reservation App",
+}
+
+func Execute() {
+	err := os.Setenv("TZ", time.UTC.String())
+	if err != nil {
+		panic(err)
+	}
+
+	err = rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("there went something wrong while loading config file")
+	}
+}
