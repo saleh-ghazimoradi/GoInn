41194dcfbbf3f6196d5d48ccbdf6da8bfa6a9f12
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-25 12:40:56 +0330

    import godotenv pakcage for handling the configuration stuff

--- config/config.go
+++ config/config.go
@@ -1,10 +1,16 @@
 package config
 
+import (
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
+	"log"
+)
+
 var AppConfig *Config
 
 type Config struct {
-	DBConfig DBConfig
-	Server   ServerConfig
+	DbConfig     DBConfig
+	ServerConfig ServerConfig
 }
 
 type DBConfig struct {
@@ -19,5 +25,28 @@ type ServerConfig struct {
 }
 
 func LoadConfig() error {
+	if err := godotenv.Load("app.env"); err != nil {
+		log.Fatal("Error loading app.env file")
+	}
+
+	config := &Config{}
+	if err := env.Parse(config); err != nil {
+		log.Fatal("Error parsing config")
+	}
+
+	serverConfig := &ServerConfig{}
+	if err := env.Parse(serverConfig); err != nil {
+		log.Fatal("Error parsing config")
+	}
+	config.ServerConfig = *serverConfig
+
+	dbConfig := &DBConfig{}
+	if err := env.Parse(dbConfig); err != nil {
+		log.Fatal("Error parsing config")
+	}
+	config.DbConfig = *dbConfig
+
+	AppConfig = config
+
 	return nil
 }
