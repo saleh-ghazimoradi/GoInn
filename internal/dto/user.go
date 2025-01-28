e963d99bfc4f28b7973677c6076950950e5d9132
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-01-28 13:00:10 +0330

    create a request model for the user in the dto directory

---
+++ internal/dto/user.go
@@ -0,0 +1,8 @@
+package dto
+
+type User struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
