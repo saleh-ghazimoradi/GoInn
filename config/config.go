ffa641e6c2a7d746b6db251ce13cb2afca286cfa
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-08-20 18:34:22 +0330

    set configs using singleton pattern

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v11"
+	"sync"
+	"time"
+)
+
+var (
+	appConfig *Config
+	once      sync.Once
+	initErr   error
+)
+
+type Config struct {
+	Server Server
+	Mongo  Mongo
+}
+
+type Server struct {
+	Host         string        `env:"SERVER_HOST"`
+	Port         string        `env:"SERVER_PORT"`
+	ReadTimeout  time.Duration `env:"SERVER_READ_TIMEOUT"`
+	WriteTimeout time.Duration `env:"SERVER_WRITE_TIMEOUT"`
+	IdleTimeout  time.Duration `env:"SERVER_IDLE_TIMEOUT"`
+	Timeout      time.Duration `env:"SERVER_TIMEOUT"`
+}
+
+type Mongo struct {
+	Host        string        `env:"MONGODB_HOST"`
+	Port        int           `env:"MONGODB_PORT"`
+	User        string        `env:"MONGODB_USER"`
+	Password    string        `env:"MONGODB_PASSWORD"`
+	Name        string        `env:"MONGODB_NAME"`
+	MaxPoolSize uint64        `env:"MONGODB_MAX_POOL_SIZE"`
+	MinPoolSize uint64        `env:"MONGODB_MIN_POOL_SIZE"`
+	Timeout     time.Duration `env:"MONGODB_TIMEOUT"`
+}
+
+func GetConfig() (*Config, error) {
+	once.Do(func() {
+		appConfig = &Config{}
+		initErr = env.Parse(appConfig)
+		if initErr != nil {
+			appConfig = nil
+		}
+	})
+	return appConfig, initErr
+}
