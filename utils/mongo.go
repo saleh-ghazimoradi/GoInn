ce1625f2a1cecc32ffb03e5119dd66e506f31ddd
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-08-20 18:32:41 +0330

    establish mogodb connection and set the configs using functional option pattern

---
+++ utils/mongo.go
@@ -0,0 +1,111 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
+	"time"
+)
+
+type MongoDB struct {
+	Host        string
+	Port        int
+	User        string
+	Pass        string
+	DBName      string
+	MaxPoolSize uint64
+	MinPoolSize uint64
+	Timeout     time.Duration
+}
+
+type Option func(*MongoDB)
+
+func WithHost(host string) Option {
+	return func(db *MongoDB) {
+		db.Host = host
+	}
+}
+
+func WithPort(port int) Option {
+	return func(db *MongoDB) {
+		db.Port = port
+	}
+}
+
+func WithUser(user string) Option {
+	return func(db *MongoDB) {
+		db.User = user
+	}
+}
+
+func WithPass(pass string) Option {
+	return func(db *MongoDB) {
+		db.Pass = pass
+	}
+}
+
+func WithDBName(dbName string) Option {
+	return func(db *MongoDB) {
+		db.DBName = dbName
+	}
+}
+
+func WithMaxPoolSize(maxPoolSize uint64) Option {
+	return func(db *MongoDB) {
+		db.MaxPoolSize = maxPoolSize
+	}
+}
+
+func WithMinPoolSize(minPoolSize uint64) Option {
+	return func(db *MongoDB) {
+		db.MinPoolSize = minPoolSize
+	}
+}
+
+func WithTimeout(timeout time.Duration) Option {
+	return func(db *MongoDB) {
+		db.Timeout = timeout
+	}
+}
+
+func (m *MongoDB) URI() string {
+	return fmt.Sprintf("mongodb://%s:%s@%s:%d/%s",
+		m.User, m.Pass, m.Host, m.Port, m.DBName)
+}
+
+func (m *MongoDB) Connect() (*mongo.Client, *mongo.Database, error) {
+	clientOpts := options.Client().ApplyURI(m.URI())
+
+	if m.MaxPoolSize > 0 {
+		clientOpts.SetMaxPoolSize(m.MaxPoolSize)
+	}
+
+	if m.MinPoolSize > 0 {
+		clientOpts.SetMinPoolSize(m.MinPoolSize)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), m.Timeout)
+
+	defer cancel()
+
+	client, err := mongo.Connect(clientOpts)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
+
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+
+	return client, client.Database(m.DBName), nil
+}
+
+func NewMongoDB(opts ...Option) *MongoDB {
+	m := &MongoDB{}
+	for _, opt := range opts {
+		opt(m)
+	}
+	return m
+}
