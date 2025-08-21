ad30a11c6585281f49d18126b375d36331f086ea
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-08-21 10:27:36 +0330

    create a JSON writer helper function

---
+++ internal/helper/writeJSON.go
@@ -0,0 +1,26 @@
+package helper
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Envelope map[string]any
+
+func WriteJOSN(w http.ResponseWriter, status int, data Envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
